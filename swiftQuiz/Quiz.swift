9df1a2a1c8fc6653230f9986a0f83a9514cb49dd
Danilo Requena <danilo.requena26@gmail.com>
2019-09-09 09:59:31 -0300

    Contrói a logica das classes QUIZ e QUIZMANAGER

--- swiftQuiz/Quiz.swift
+++ swiftQuiz/Quiz.swift
@@ -10,5 +10,23 @@ import Foundation
 
 
 class Quiz {
+    let question: String
+    let options: [String]
+    private let correctedAnswers: String
     
+    init(question: String, options: [String], correctedAnswers: String) {
+        self.question = question
+        self.options = options
+        self.correctedAnswers = correctedAnswers
+    }
+    
+    func validateOption(_ index: Int) -> Bool {
+        let answer = options[index]
+        
+        return answer == correctedAnswers
+    }
+    
+    deinit {
+        print("Liberou o quiz da memória")
+    }
 }
